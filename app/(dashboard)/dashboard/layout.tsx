ab11b05b185e2527e86744cb515d53ed53e78299
joaovrcosta <joaovr.costa@gmail.com>
2025-08-01 12:26:08 -0300

    first commit

---
+++ app/(dashboard)/dashboard/layout.tsx
@@ -0,0 +1,22 @@
+import { AppSidebar } from "@/components/app-sidebar";
+import {
+  Sidebar,
+  SidebarProvider,
+  SidebarTrigger,
+} from "@/components/ui/sidebar";
+
+export default async function DashboardLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <SidebarProvider>
+      <AppSidebar />
+      <main>
+        <SidebarTrigger />
+        {children}
+      </main>
+    </SidebarProvider>
+  );
+}
---
+++ components/app-sidebar.tsx
@@ -0,0 +1,66 @@
+import { Calendar, Home, Inbox, Search, Settings } from "lucide-react";
+
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from "@/components/ui/sidebar";
+
+const items = [
+  {
+    title: "Estatísticas",
+    url: "#",
+    icon: Home,
+  },
+  {
+    title: "Force1",
+    url: "#",
+    icon: Inbox,
+  },
+  {
+    title: "Alaska",
+    url: "#",
+    icon: Calendar,
+  },
+  {
+    title: "Crystal",
+    url: "#",
+    icon: Search,
+  },
+  {
+    title: "Bluesky",
+    url: "#",
+    icon: Settings,
+  },
+];
+
+export function AppSidebar() {
+  return (
+    <Sidebar>
+      <SidebarContent>
+        <SidebarGroup>
+          <SidebarGroupLabel>Application</SidebarGroupLabel>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              {items.map((item) => (
+                <SidebarMenuItem key={item.title}>
+                  <SidebarMenuButton asChild>
+                    <a href={item.url}>
+                      <item.icon />
+                      <span>{item.title}</span>
+                    </a>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+              ))}
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+      </SidebarContent>
+    </Sidebar>
+  );
+}
